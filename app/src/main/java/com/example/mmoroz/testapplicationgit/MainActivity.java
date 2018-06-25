b91ca85c01bca9eb572135ba6f2e7ff9bb9bc1fd
maksym.s.moroz <maksym.s.moroz@globallogic.com>
2018-06-25 16:50:00 +0300

    Added new SetupActivity

--- app/src/main/java/com/example/mmoroz/testapplicationgit/MainActivity.java
+++ app/src/main/java/com/example/mmoroz/testapplicationgit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mmoroz.testapplicationgit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),
                         myButton.getText().toString(), Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), SetupActivity.class);
+                intent.putExtra("text", myButton.getText().toString());
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/mmoroz/testapplicationgit/SetupActivity.java
@@ -0,0 +1,22 @@
+package com.example.mmoroz.testapplicationgit;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText(getIntent().getExtras().getString("text"));
+    }
+
+}
