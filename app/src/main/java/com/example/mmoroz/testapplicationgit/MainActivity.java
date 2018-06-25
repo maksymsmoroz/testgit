351246a776912d9f887239070aa10520b6fc85b9
maksym.s.moroz <maksym.s.moroz@globallogic.com>
2018-06-25 16:22:35 +0300

    changes to mainactivity layout

--- app/src/main/java/com/example/mmoroz/testapplicationgit/MainActivity.java
+++ app/src/main/java/com/example/mmoroz/testapplicationgit/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.mmoroz.testapplicationgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button) findViewById(R.id.center_button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        myButton.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
